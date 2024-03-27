2e16a2eea1051370ea9c566638a442cc44d92a64
FrancoCamen <francocamen1@gmail.com>
2024-03-27 18:55:22 -0300

    Se modifico factorial.py para admitir un rango de numeros

--- src/factorial.py
+++ src/factorial.py
@@ -6,13 +6,12 @@
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
 import sys
+
 def factorial(num):     
     if num < 0: 
-        print("Factorial de un numero negativo no existe")
-
+        print("Factorial de un número negativo no existe")
     elif num == 0: 
         return 1
-        
     else: 
         fact = 1
         while(num > 1): 
@@ -20,17 +19,23 @@ def factorial(num):
             num -= 1
         return fact 
 
+def calcular_factorial_entre_rango(desde, hasta):
+    for num in range(desde, hasta + 1):
+        print("Factorial de", num, "! es", factorial(num))
 
 while True:
     if len(sys.argv) == 1:
         try:
-            num = int(input("Por favor, ingrese un numero: "))
-            print("Factorial", num, "! es", factorial(num))
+            rango = input("Por favor, ingrese un rango de números en el formato desde-hasta (ej. 4-8): ")
+            desde, hasta = map(int, rango.split("-"))
+            calcular_factorial_entre_rango(desde, hasta)
             break
         except ValueError:
-            print("¡Debe ingresar un numero entero!")
+            print("¡Formato incorrecto! Debe ingresar un rango válido en el formato desde-hasta.")
     else:
-        num = int(sys.argv[1])
-        print("Factorial", num, "! es", factorial(num))
+        rango = sys.argv[1]
+        desde, hasta = map(int, rango.split("-"))
+        calcular_factorial_entre_rango(desde, hasta)
         break
+
 
